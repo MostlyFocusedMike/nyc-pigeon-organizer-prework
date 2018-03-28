53fabb002e901223089985d84cb8d2dbc46ed80e
Mike C <mikecronin92@gmail.com>
2018-03-28 19:15:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,3 +10,16 @@ def nyc_pigeon_organizer(data)
   end
   return new_hash
 end
+
+def nyc_pigeon_organizer1(data)
+  new_hash = {}
+  data.each do |stat,hash|
+    hash.each do |specific,birds| #purple
+      birds.each do |bird|
+        new_hash[bird] ||= {color: [], gender: [], lives: []}
+        new_hash[bird][stat] << specific.to_s 
+      end
+    end
+  end
+  return new_hash
+end
