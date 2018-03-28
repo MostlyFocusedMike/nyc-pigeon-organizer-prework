6bd9a051347bd359483163f764bdd541f53c77c7
Mike C <mikecronin92@gmail.com>
2018-03-28 19:43:18 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,9 @@
 def nyc_pigeon_organizer(data)
+  data.each_with_object({})
+end
+
+
+def nyc_pigeon_organizer2(data)
   new_hash = {}
   data.each do |stat,hash|
     hash.each do |specific,birds| #purple
